63de745bc9cf0654c2717257cad3a723b70fd5d2
John Bond <github@johnbond.org>
2021-07-14 01:43:21 +0200

    commit #2: add basic module structure
    This commit adds the basic structure needed to create your first puppet
    modules.  the key thing here is when calling either of the following the
    code in modules/myfunckyservice/manifests/init.pp is what will get run
    
    ```puppet
     # include the class can be called multiple times
    include myfunckyservice
     # Ensure all code in myfunckyservice is run before any statments which
     # follow
    contain myfunckyservice
     # can only be called once in a manifest
    class {'myfunckyservice': }
    ```

---
+++ modules/myfunckyservice/manifests/init.pp
@@ -0,0 +1,10 @@
+# This name needs to match the module name i.e. the folder that exists under modules
+# init.pp is a special file which will always be used when some one includes or instantiates a class e.g.
+# class {'myfunckyservice'} or include myfunckyservice
+class myfunckyservice (
+) {
+
+  # Install the applications
+  # write our the config file
+  # manage the service
+}
