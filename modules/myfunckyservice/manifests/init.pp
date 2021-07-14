544ce19ae071a7a190640d1160093cb30d088f68
John Bond <github@johnbond.org>
2021-07-14 02:00:56 +0200

    commit #3: this commit installs the main package
    The main take way from this is use `ensure_packages()` to over the
    package type unless you have a reason not to

--- modules/myfunckyservice/manifests/init.pp
+++ modules/myfunckyservice/manifests/init.pp
@@ -1,10 +1,18 @@
-# This name needs to match the module name i.e. the folder that exists under modules
-# init.pp is a special file which will always be used when some one includes or instantiates a class e.g.
-# class {'myfunckyservice'} or include myfunckyservice
 class myfunckyservice (
 ) {
-
-  # Install the applications
+  # tl;dr prefer ensure_packages to using the package type directly unless
+  # you have a valid reason.
+  # we could use the package resource directly with something like
+  # package{'myfunckyservice':
+  #   ensure => 'installed'
+  # }
+  # however it is quite common for multiple modules to try and install the same package.
+  # In puppet all resources have to be unique i.e. two instance of
+  # package{'myfunckyservice'} in the same catalog will cause an error.
+  # using ensure_packages gracefully handles this and ensures only one
+  # package{'myfunckyservice'} exists in the manifest no matter how many modules
+  # call ensure_packages('myfunckyservice')
+  ensure_packages('myfunckyservice')
   # write our the config file
   # manage the service
 }
