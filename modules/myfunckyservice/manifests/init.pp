f11481037fd481953b8d21fb753d79f3ad675b35
John Bond <github@johnbond.org>
2021-07-14 03:16:30 +0200

    commit #7: do we really need ERB
    In this commit we drop erb as its much in this case we have a well
    structured config file and its much easier to just create a local
    variable with the correct data structure then trying to write valid yaml
    with ERB
    
    We also add some additional type checking to the access_list variable

--- modules/myfunckyservice/manifests/init.pp
+++ modules/myfunckyservice/manifests/init.pp
@@ -3,39 +3,39 @@ class myfunckyservice (
   String           $api_key,
   # this is another stdlib type see if you can find the definition
   # (hint in the types dir of the stdlib module)
-  Stdlib::Httpurl  $api_uri,
-  String           $owner       = 'root',
-  String           $group       = 'root',
-  Stdlib::Filemode $mode        = '0500',
-  # By using Array and a default of [] we essentially make this param
-  # optional. will cover more advance use cases in further commits
-  Array            $access_list = [],
-  # similar to above we default this to an empty hash
-  Hash             $datasets    = {},
+  Stdlib::Httpurl            $api_uri,
+  String                     $owner       = 'root',
+  String                     $group       = 'root',
+  Stdlib::Filemode           $mode        = '0500',
+  # We know that we want an array of ip addresses so lets make this define stricter
+  # Stdlib::IP::Address comes is another custom type from stdlib.  It matches any valid
+  # ipv4/6 ip address or prefix.  its a fairly complex type with some horrible regex but
+  # a good example of what custome types are capable of
+  # https://github.com/puppetlabs/puppetlabs-stdlib/blob/main/types/ip/address.pp
+  # (you will need to recurse into all the variant types to get a full grasp)
+  Array[Stdlib::IP::Address] $access_list = [],
+  # We also make this a bit stricter however in the next commit we will add a custom type here
+  Hash[String, Hash]         $datasets    = {},
 ) {
   $config_file = '/etc/myfunckyservice/config.yaml'
+  # its very common to see software use a yaml config file, as this is structured
+  # data its much easier to create the necessary structure and just covert it to yaml
+  # instead of trying to write out valid yaml in erb
+  $config = {
+    'api_key'     => $api_key,
+    'api_uri'     => $api_uri,
+    'access_list' => $access_list,
+    'datasets'    => $datasets,
+  }
   ensure_packages('myfunckyservice')
   file {$config_file:
     ensure  => file,
     owner   => $owner,
     group   => $group,
     mode    => $mode,
-    # we change this here to use the content param and the template function.
-    # When using the source parameter puppet buts a reference to a file source location
-    # in the catalog.  This can be a http address a local file one the host or a puppet uri
-    # (by far the most common).  This instructs the puppet agent to fetch the file as it processes
-    # the catalog.
-    # the content parameter on the other hand tags a string and ads the raw string directly into the
-    # catalog (this means you can see diffs in pcc when using content but you cant using source)
-    #
-    # The template function pareses an erb file and return the parsed template as a string
-    # notice in this case we have to use the module name in the call to template and we drop
-    # the word 'modules'.  this is jut the way it is.  this will expect to fine a template in
-    # modules/myfunckyservice/templates/config.yaml.erb
-    # if we left out the module name i.e. `template('config.yaml.erb')` the template would be
-    # pulled from `templates/config.yaml.erb` there is no templates dir in the ops puppet control
-    # repo and you are unlikely to ever need to put  template file there
-    content => template('myfunckyservice/config.yaml.erb'),
+    # `to_yaml` is a function provided by stdlib which take a puppet variables and
+    # returns a valid yaml as a string
+    content => $config.to_yaml,
     require => Package['myfunckyservice'],
   }
   # manage the service
