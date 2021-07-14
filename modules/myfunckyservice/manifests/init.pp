4bc64a53cf659b93882781b31cf1df888f761913
John Bond <github@johnbond.org>
2021-07-14 13:15:18 +0200

    commit #10: lets make things ensureable
    When we look at adding this to a class to the profile/role structure
    later it could be tempting to make the inclusion optional with something
    like
    
    ```puppet
    if $enable_myfunckyservice {
      include myfunckyservice
    }
    ```
    
    This allows on to only enable myfunckyservice on some hosts based on the
    boolean value of `$enable_myfunckyservice`.  This works fine for slowly
    introducing a service however it dosn;t actully allow is to disable the
    service after the fact.  Setting `$enable_myfunckyservice = false` will
    simply mean that the class is not included in the manifest and that any
    resources it manages are no longer managed.  however
     * any started services will still be started
     * installed packages will still be installed
     * and any files created will still exists
    
    Makeing a class ensureable $ensure fixes this issue.  We make a class
    ensureable by adding and implementing the $ensure parameter.  In most
    cases we can either pass the ensure parameter directly to all/most
    managed resources or use the `stdlib::ensure` function for more
    flexibility

--- modules/myfunckyservice/manifests/init.pp
+++ modules/myfunckyservice/manifests/init.pp
@@ -1,12 +1,14 @@
 class myfunckyservice (
   String                                 $api_key,
   Stdlib::Httpurl                        $api_uri,
+  # Wmflib::Ensure is a custom type provided by the wmflib module
+  # this is just an alias to Enum['present', 'absent']
+  # https://github.com/wikimedia/puppet/blob/production/modules/wmflib/types/ensure.pp
+  Wmflib::Ensure                         $ensure      = 'present',
   String                                 $owner       = 'root',
   String                                 $group       = 'root',
   Stdlib::Filemode                       $mode        = '0500',
   Array[Stdlib::IP::Address]             $access_list = [],
-  # As we know whet the data structre for datasets should look like
-  # lets use a custom type and validate it
   Hash[String, Myfunckyservice::Dataset] $datasets    = {},
 ) {
   $config_file = '/etc/myfunckyservice/config.yaml'
@@ -16,25 +18,48 @@ class myfunckyservice (
     'access_list' => $access_list,
     'datasets'    => $datasets,
   }
-  ensure_packages('myfunckyservice')
+  # ensure_packages takes an optional second argument which can be used to override
+  # the default options passed to the package type.  here we use it to pass the ensure
+  # parameter.  It is worth noting ultimatly all definitions of the `myfunckyservice`
+  # package must be defined with the same set of parameteres every where.  the fuction below
+  # will create the following resource
+  # package{'myfunckyservice':
+  #   ensure => present
+  # }
+  # if we have a definitions for ensure_packages('myfunckyservice') else where in the manifest
+  # and we set this classes parameter to 'absent' then we would end up with conflict as the
+  # ensure parmeter of the two definitions don't match for this reason and its often desirable to
+  # skip making the package install ensurable and just live with the fact that we have some extra
+  # packages installed.  this is something left to to the module authors
+  ensure_packages('myfunckyservice', {'ensure' => $ensure})
   file {$config_file:
-    ensure  => file,
+    # Stdlib::ensure is a function which takes the following types
+    # Variant[Boolean, Enum[''abesnt', 'present']
+    # it then returns the resources specific ensure value.  in this case it will return
+    #   * 'file' -  when passed either present of true
+    #   * 'absent' -  when passed either absent of false
+    # This helper function allows use to use the same ensure value on many resources.
+    # in most cases you can also simply pass $ensure directly and puppet
+    # will do the right thing.  however we can be a bit more specific with the helper function
+    # for instance if we simply pass 'present' to the ensure value of the file resource then puppet
+    # will just ensure there is a file like object which could be a directory, link of file.
+    # By passing a value of 'file' as this helper function dose we ensure the resource create is a real file
+    ensure  => stdlib::ensure($ensure, 'file'),
     owner   => $owner,
     group   => $group,
     mode    => $mode,
     content => $config.to_yaml,
     require => Package['myfunckyservice'],
-    # the notify keeword is used to "refresh" another resource,  not all resources
-    # are refreshable and the ones that are do different things depending on the type.
-    # when refreshing (sending a notify) to a service resource on a sytemd system it will do
-    # `systemctl restart $service` which in the vast majority of cases is what you want to do
     notify  => Service['myfunckyservice'],
   }
   service {'myfunckyservice':
-    ensure      => running,
-    enabled     => true,
-    # we could use subscribe here like the example below but notify
-    # like `require` is more common
-    # subscribe => File[$config_file]
+    # In this case it will return
+    #   * 'running' -  when passed either present of true
+    #   * 'stopped' -  when passed either absent of false
+    # In the specific case of the service resource we note that that it dosn't accept
+    # ensure values of 'present', 'absent' as such passing through the ensure value without
+    # the helper function or some other manipulation then compilation would fail
+    ensure  => stdlib::ensure($ensure, 'service'),
+    enabled => true,
   }
 }
