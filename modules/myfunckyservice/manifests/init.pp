27d920578c7759ab1907a9b8ea12ce429cd9dfd5
John Bond <github@johnbond.org>
2021-07-14 13:34:16 +0200

    commit 11#: I love it, you <3, it we all ❤ it! Documentation
    This commit introduces the puppet-strings format for documenting your
    modules.  There is not much more to say, i think the format is fairly
    self explanatory

--- modules/myfunckyservice/manifests/init.pp
+++ modules/myfunckyservice/manifests/init.pp
@@ -1,10 +1,24 @@
+# @summary A simple class example class
+#  the comments here use YARD tags to document the class, more information on the avalible tags
+#  can be found on the puppet labs docs
+#  https://puppet.com/docs/puppet/7/puppet_strings.html
+# @param api_key the apik key for service
+# @param api_uri the uri for service
+# @param ensure the ensureable parameter
+# @param owner all files created by this module will be owned by this user
+# @param group all files created by this module will be owned by this group
+# @param mode all files created by this module will be managed with this mode
+# @param access_list an array of ip addresses by myfunckyservice
+# @param datasets an hash of datasets
+# @example
+#  class {'myfunckyservice':
+#    api_key => 'secret_key',
+#  }
 class myfunckyservice (
   String                                 $api_key,
-  Stdlib::Httpurl                        $api_uri,
-  # Wmflib::Ensure is a custom type provided by the wmflib module
-  # this is just an alias to Enum['present', 'absent']
-  # https://github.com/wikimedia/puppet/blob/production/modules/wmflib/types/ensure.pp
   Wmflib::Ensure                         $ensure      = 'present',
+  # We should have given this a sane default much earlier
+  Stdlib::Httpurl                        $api_uri     = 'https://mfs.example.org',
   String                                 $owner       = 'root',
   String                                 $group       = 'root',
   Stdlib::Filemode                       $mode        = '0500',
@@ -18,32 +32,8 @@ class myfunckyservice (
     'access_list' => $access_list,
     'datasets'    => $datasets,
   }
-  # ensure_packages takes an optional second argument which can be used to override
-  # the default options passed to the package type.  here we use it to pass the ensure
-  # parameter.  It is worth noting ultimatly all definitions of the `myfunckyservice`
-  # package must be defined with the same set of parameteres every where.  the fuction below
-  # will create the following resource
-  # package{'myfunckyservice':
-  #   ensure => present
-  # }
-  # if we have a definitions for ensure_packages('myfunckyservice') else where in the manifest
-  # and we set this classes parameter to 'absent' then we would end up with conflict as the
-  # ensure parmeter of the two definitions don't match for this reason and its often desirable to
-  # skip making the package install ensurable and just live with the fact that we have some extra
-  # packages installed.  this is something left to to the module authors
   ensure_packages('myfunckyservice', {'ensure' => $ensure})
   file {$config_file:
-    # Stdlib::ensure is a function which takes the following types
-    # Variant[Boolean, Enum[''abesnt', 'present']
-    # it then returns the resources specific ensure value.  in this case it will return
-    #   * 'file' -  when passed either present of true
-    #   * 'absent' -  when passed either absent of false
-    # This helper function allows use to use the same ensure value on many resources.
-    # in most cases you can also simply pass $ensure directly and puppet
-    # will do the right thing.  however we can be a bit more specific with the helper function
-    # for instance if we simply pass 'present' to the ensure value of the file resource then puppet
-    # will just ensure there is a file like object which could be a directory, link of file.
-    # By passing a value of 'file' as this helper function dose we ensure the resource create is a real file
     ensure  => stdlib::ensure($ensure, 'file'),
     owner   => $owner,
     group   => $group,
@@ -53,12 +43,6 @@ class myfunckyservice (
     notify  => Service['myfunckyservice'],
   }
   service {'myfunckyservice':
-    # In this case it will return
-    #   * 'running' -  when passed either present of true
-    #   * 'stopped' -  when passed either absent of false
-    # In the specific case of the service resource we note that that it dosn't accept
-    # ensure values of 'present', 'absent' as such passing through the ensure value without
-    # the helper function or some other manipulation then compilation would fail
     ensure  => stdlib::ensure($ensure, 'service'),
     enabled => true,
   }
